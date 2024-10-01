e9d89a058a15957c806bcf3a870c4d7275c4397d
GeoJere01 <georgejere60@gmail.com>
2024-09-30 19:48:41 -0700

    second commit

---
+++ src/constants/index.js
@@ -0,0 +1,288 @@
+export const navLinks = [
+    {
+        id: 1,
+        name: 'Home',
+        href: '#home',
+    },
+    {
+        id: 2,
+        name: 'About',
+        href: '#about',
+    },
+    {
+        id: 3,
+        name: 'Work',
+        href: '#work',
+    },
+    {
+        id: 4,
+        name: 'Contact',
+        href: '#contact',
+    },
+];
+
+export const clientReviews = [
+    {
+        id: 1,
+        name: 'Hartson Lebium Doviko',
+        position: 'Finanial Controller at Nexus-Tech',
+        img: 'assets/jere.png',
+        review:
+            'Working with George was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+    },
+    {
+        id: 2,
+        name: 'Charles Mitiwe',
+        position: 'Designer of Nexus-Tech',
+        img: 'assets/jere.png',
+        review:
+            'George’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+    },
+    {
+        id: 3,
+        name: 'Samson Chinthemga',
+        position: 'Project Manager at Nexus-Tech',
+        img: 'assets/jere.png',
+        review:
+            'I can’t say enough good things about George. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+    },
+    {
+        id: 4,
+        name: 'Monawe Isaac',
+        position: 'CEO of Nexus-Tech',
+        img: 'assets/jere.png',
+        review:
+            'George was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+    },
+];
+
+export const myProjects = [
+    {
+        title: 'Share Prompts - AI Prompts Platform',
+        desc: 'Share Prompts is an AI prompts platform that empowers users to generate prompts using ChatGPT and generates unique prompts for various topics.',
+        subdesc:
+            'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript,it is designed for optimal performance and scalability.',
+        href: '/share-prompts',
+        texture: '/textures/project/project1.mp4',
+        logo: '/assets/project-logo1.png',
+        logoStyle: {
+            backgroundColor: '#2A1816',
+            border: '0.2px solid #36201D',
+            boxShadow: '0px 0px 60px 0px #AA3C304D',
+        },
+        spotlight: '/assets/spotlight1.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: 'Pricewise - Amozone web scraping with Bright data',
+        desc: 'Amazon product tracking is a web scraping app that helps users to track their Amazon products. It is a unique product tracking app that allows users to track products in real-time.',
+        subdesc:
+            'Built with Next.js 14, Bright data, and Stripe, Amazon product tracking ensures a smooth and secure product tracking experience, tailored to meet the needs of modern consumers.',
+        href: 'https://pricewise-12eeskre0-george-jeres-projects.vercel.app/',
+        texture: '/textures/project/project2.mp4',
+        logo: '/assets/project-logo2.png',
+        logoStyle: {
+            backgroundColor: '#13202F',
+            border: '0.2px solid #17293E',
+            boxShadow: '0px 0px 60px 0px #2F6DB54D',
+        },
+        spotlight: '/assets/spotlight2.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: 'Football Analysis - player tracking',
+        desc: 'Football Analysis is a computer vision app that helps users to track their favorite football players. It is a unique player tracking app that allows users to track players in real-time.',
+        subdesc:
+            'Built with Next.js 14, Framer Motion, and Stripe, Football Analysis ensures a smooth and secure player tracking experience, tailored to meet the needs of modern consumers.',
+        href: 'https://football-analysis-george-jeres-projects.vercel.app/',
+        texture: '/textures/project/futbo.avi',
+        logo: '/assets/project-logo3.png',
+        logoStyle: {
+            backgroundColor: '#60f5a1',
+            background:
+                'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+            border: '0.2px solid rgba(208, 213, 221, 1)',
+            boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+        },
+        spotlight: '/assets/spotlight3.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: 'Packing management - Real-time car packing management system',
+        desc: 'Packing management is an app that helps users to manage their car packing. It is a unique car packing management app that allows users to manage cars in real-time.',
+        subdesc:
+            'Built with Next.js 14, Framer Motion, and Stripe, Packing management ensures a smooth and secure car packing management experience, tailored to meet the needs of modern consumers.',
+        href: 'https://packing-management-george-jeres-projects.vercel.app/',
+        texture: '/textures/project/pack_them.mp4',
+        logo: '/assets/project-logo4.png',
+        logoStyle: {
+            backgroundColor: '#0E1F38',
+            border: '0.2px solid #0E2D58',
+            boxShadow: '0px 0px 60px 0px #2F67B64D',
+        },
+        spotlight: '/assets/spotlight4.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+    {
+        title: '3D Dev portfoilio',
+        desc: 'It is a unique 3D portfolio app that allows users to create their own 3D portfolio in real-time.',
+        subdesc:
+            'Built with Next.js 14, Framer Motion, and Stripe, 3D Dev ensures a smooth and secure 3D portfolio experience, tailored to meet the needs of modern consumers.',
+        href: 'https://3d-dev-george-jeres-projects.vercel.app/',
+        texture: '/textures/project/project5.mp4',
+        logo: '/assets/project-logo5.png',
+        logoStyle: {
+            backgroundColor: '#1C1A43',
+            border: '0.2px solid #252262',
+            boxShadow: '0px 0px 60px 0px #635BFF4D',
+        },
+        spotlight: '/assets/spotlight5.png',
+        tags: [
+            {
+                id: 1,
+                name: 'React.js',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'TailwindCSS',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'TypeScript',
+                path: '/assets/typescript.png',
+            },
+            {
+                id: 4,
+                name: 'Framer Motion',
+                path: '/assets/framer.png',
+            },
+        ],
+    },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+        deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.055,
+        deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+        cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+        reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+        ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+        targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+};
+
+export const workExperiences = [
+    {
+        id: 1,
+        name: 'Framer',
+        pos: 'Lead Web Developer',
+        duration: '2022 - Present',
+        title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+        icon: '/assets/framer.svg',
+        animation: 'victory',
+    },
+    {
+        id: 2,
+        name: 'Figma',
+        pos: 'Web Developer',
+        duration: '2020 - 2022',
+        title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+        icon: '/assets/figma.svg',
+        animation: 'clapping',
+    },
+    {
+        id: 3,
+        name: 'Notion',
+        pos: 'Junior Web Developer',
+        duration: '2019 - 2020',
+        title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+        icon: '/assets/notion.svg',
+        animation: 'salute',
+    },
+];
