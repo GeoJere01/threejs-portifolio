81d83c0f6265159121e00e78e525cfa6b71ea1ed
GeoJere01 <georgejere60@gmail.com>
2025-04-22 14:41:10 -0700

    Changed hero section features

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,18 +1,19 @@
-import { Leva } from 'leva';
-import { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { useMediaQuery } from 'react-responsive';
-import { PerspectiveCamera } from '@react-three/drei';
+// import { Leva } from "leva";
+// import { Suspense } from "react";
+// import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
+// import { PerspectiveCamera } from "@react-three/drei";
+import { Typewriter } from "react-simple-typewriter";
 
-import Cube from '../components/Cube.jsx';
-import Rings from '../components/Rings.jsx';
-import ReactLogo from '../components/ReactLogo.jsx';
-import Button from '../components/Button.jsx';
-import Target from '../components/Target.jsx';
-import CanvasLoader from '../components/CanvasLoader.jsx';
-import HeroCamera from '../components/HeroCamera.jsx';
-import { calculateSizes } from '../constants/index.js';
-import HackerRoom from '/src/components/HackerRoom.jsx';
+// import Cube from "../components/Cube.jsx";
+// import Rings from "../components/Rings.jsx";
+// import ReactLogo from "../components/ReactLogo.jsx";
+import Button from "../components/Button.jsx";
+// import Target from "../components/Target.jsx";
+// import CanvasLoader from "../components/CanvasLoader.jsx";
+// import HeroCamera from "../components/HeroCamera.jsx";
+import { calculateSizes } from "../constants/index.js";
+// import HackerRoom from "/src/components/HackerRoom.jsx";
 
 const Hero = () => {
   // Use media queries to determine screen size
@@ -23,45 +24,83 @@ const Hero = () => {
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
-    <section className="min-h-screen w-full flex flex-col relative" id="home">
+    <section
+      className="bg-[url(/assets/back-g.jpg)] bg-cover bg-center bg-no-repeat min-h-screen w-full flex flex-col relative"
+      id="home"
+    >
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I am George Jere <span className="waving-hand">👋</span>
+        {/* <h1 className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans pt-6">
+          Hey, I am George Jere{" "}
+          <span className="text-yellow-500 font-semibold">
+            <Typewriter
+              words={["Eat", "Sleep", "Breathe"]}
+              loop={5}
+              cursor={true}
+              cursorStyle=""
+              typeSpeed={60}
+              delaySpeed={50}
+              deleteSpeed={1000}
+            />
+          </span>{" "}
+          React!
+        </h1> */}
+        <p className="text-white xl:text-8xl md:text-7xl sm:text-6xl text-5xl text-center font-generalsans font-black !leading-normal pt-20">
+          Hey, I am George Jere.
         </p>
-        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+        <h3 className="text-white text-center xl:text-5xl md:text-4xl sm:text-3xl text-2xl">
+          <span className="text-white-700 font-semibold">
+            <Typewriter
+              words={["Code.", "Rest.", "Repeat!"]}
+              loop={false}
+              cursor={true}
+              cursorStyle=""
+              typeSpeed={60}
+              delaySpeed={50}
+              deleteSpeed={1000}
+            />
+          </span>
+        </h3>
       </div>
 
-      <div className="w-full h-full absolute inset-0">
-        <Canvas className="w-full h-full">
-          <Suspense fallback={<CanvasLoader />}>
-            {/* To hide controller */}
-            <Leva hidden />
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-
-            <HeroCamera isMobile={isMobile}>
-              <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
-            </HeroCamera>
-
-            <group>
-              <Target position={sizes.targetPosition} />
-              <ReactLogo position={sizes.reactLogoPosition} />
-              <Rings position={sizes.ringPosition} />
-              <Cube position={sizes.cubePosition} />
-            </group>
-
-            <ambientLight intensity={1} />
-            <directionalLight position={[10, 10, 10]} intensity={0.5} />
-          </Suspense>
-        </Canvas>
-      </div>
+      {/* {newFunction()} */}
 
       <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
         <a href="#about" className="w-fit">
-          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+          <Button
+            name="Let's work together"
+            isBeam
+            containerClass="sm:w-fit w-full sm:min-w-96"
+          />
         </a>
       </div>
     </section>
   );
+
+  //   function newFunction() {
+  //     return <div className="w-full h-full absolute inset-0">
+  //       <Canvas className="w-full h-full">
+  //         <Suspense fallback={<CanvasLoader />}>
+  //           {/* To hide controller */}
+  //           <Leva hidden />
+  //           <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+
+  //           {/* <HeroCamera isMobile={isMobile}>
+  //           <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
+  //         </HeroCamera> */}
+
+  //           <group>
+  //             <Target position={sizes.targetPosition} />
+  //             <ReactLogo position={sizes.reactLogoPosition} />
+  //             <Rings position={sizes.ringPosition} />
+  //             <Cube position={sizes.cubePosition} />
+  //           </group>
+
+  //           <ambientLight intensity={1} />
+  //           <directionalLight position={[10, 10, 10]} intensity={0.5} />
+  //         </Suspense>
+  //       </Canvas>
+  //     </div>;
+  //   }
 };
 
 export default Hero;
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,12 +1,12 @@
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { Suspense, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Center, OrbitControls } from '@react-three/drei';
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useState } from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { Center, OrbitControls } from "@react-three/drei";
 
-import { myProjects } from '../constants/index.js';
-import CanvasLoader from '../components/CanvasLoader.jsx';
-import DemoComputer from '../components/DemoComputer.jsx';
+import { myProjects } from "../constants/index.js";
+// import CanvasLoader from "../components/CanvasLoader.jsx";
+// import DemoComputer from "../components/DemoComputer.jsx";
 
 const projectCount = myProjects.length;
 
@@ -15,7 +15,7 @@ const Projects = () => {
 
   const handleNavigation = (direction) => {
     setSelectedProjectIndex((prevIndex) => {
-      if (direction === 'previous') {
+      if (direction === "previous") {
         return prevIndex === 0 ? projectCount - 1 : prevIndex - 1;
       } else {
         return prevIndex === projectCount - 1 ? 0 : prevIndex + 1;
@@ -24,7 +24,11 @@ const Projects = () => {
   };
 
   useGSAP(() => {
-    gsap.fromTo(`.animatedText`, { opacity: 0 }, { opacity: 1, duration: 1, stagger: 0.2, ease: 'power2.inOut' });
+    gsap.fromTo(
+      `.animatedText`,
+      { opacity: 0 },
+      { opacity: 1, duration: 1, stagger: 0.2, ease: "power2.inOut" }
+    );
   }, [selectedProjectIndex]);
 
   const currentProject = myProjects[selectedProjectIndex];
@@ -33,18 +37,37 @@ const Projects = () => {
     <section className="c-space my-20">
       <p className="head-text">My Selected Work</p>
 
-      <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
-        <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
+      {newFunction()}
+    </section>
+  );
+
+  function newFunction() {
+    return (
+      <div className="mt-12 gap-5 w-full">
+        <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200 max-w-full">
           <div className="absolute top-0 right-0">
-            <img src={currentProject.spotlight} alt="spotlight" className="w-full h-96 object-cover rounded-xl" />
+            <img
+              src={currentProject.spotlight}
+              alt="spotlight"
+              className="w-full h-96 object-cover rounded-xl"
+            />
           </div>
 
-          <div className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg" style={currentProject.logoStyle}>
-            <img className="w-10 h-10 shadow-sm" src={currentProject.logo} alt="logo" />
+          <div
+            className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg"
+            style={currentProject.logoStyle}
+          >
+            <img
+              className="w-10 h-10 shadow-sm"
+              src={currentProject.logo}
+              alt="logo"
+            />
           </div>
 
           <div className="flex flex-col gap-5 text-white-600 my-5">
-            <p className="text-white text-2xl font-semibold animatedText">{currentProject.title}</p>
+            <p className="text-white text-2xl font-semibold animatedText">
+              {currentProject.title}
+            </p>
 
             <p className="animatedText">{currentProject.desc}</p>
             <p className="animatedText">{currentProject.subdesc}</p>
@@ -63,40 +86,44 @@ const Projects = () => {
               className="flex items-center gap-2 cursor-pointer text-white-600"
               href={currentProject.href}
               target="_blank"
-              rel="noreferrer">
+              rel="noreferrer"
+            >
               <p>Check Live Site</p>
               <img src="/assets/arrow-up.png" alt="arrow" className="w-3 h-3" />
             </a>
           </div>
 
           <div className="flex justify-between items-center mt-7">
-            <button className="arrow-btn" onClick={() => handleNavigation('previous')}>
+            <button
+              className="arrow-btn"
+              onClick={() => handleNavigation("previous")}
+            >
               <img src="/assets/left-arrow.png" alt="left arrow" />
             </button>
 
-            <button className="arrow-btn" onClick={() => handleNavigation('next')}>
-              <img src="/assets/right-arrow.png" alt="right arrow" className="w-4 h-4" />
+            <button
+              className="arrow-btn"
+              onClick={() => handleNavigation("next")}
+            >
+              <img
+                src="/assets/right-arrow.png"
+                alt="right arrow"
+                className="w-4 h-4"
+              />
             </button>
           </div>
         </div>
 
-        <div className="border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
-          <Canvas>
-            <ambientLight intensity={Math.PI} />
-            <directionalLight position={[10, 10, 5]} />
-            <Center>
-              <Suspense fallback={<CanvasLoader />}>
-                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
-                  <DemoComputer texture={currentProject.texture} />
-                </group>
-              </Suspense>
-            </Center>
-            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
-          </Canvas>
-        </div>
+        {/* <div className="flex justify-center items-center border border-black-300 bg-black-200 rounded-lg h-96 md:h-full">
+          <img
+            src={currentProject.texture}
+            alt="grid-2"
+            className="max-h-full max-w-[90%] object-contain m-4"
+          />grid lg:grid-cols-2 grid-cols-1 --> for the upper div to divide the compoent into two sections
+        </div> */}
       </div>
-    </section>
-  );
+    );
+  }
 };
 
 export default Projects;
